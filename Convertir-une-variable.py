c29940778a1b3ee5732fd258249909341bdcefa7
yogisen <36984500+yogisen@users.noreply.github.com>
2018-10-13 07:12:01 +0200

    Add files via upload

---
+++ Convertir-une-variable.py
@@ -0,0 +1,14 @@
+# Pour convertir une variable d'un type
+# a un autre, on peut utiliser les fonctions
+# str(), int() et float()
+
+# Variable de type string
+mon_nom = 'Yogi'
+# Variable de type integer
+mon_age = 35
+
+# On convertie la variable de type integer en string afin
+# de pouvoir la concatener avec les autres variables.
+print("Je m'appelle " + mon_nom + " et j'ai " + str(mon_age) + " ans.")
+print (mon_nom)
+print (mon_age)
