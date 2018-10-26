4b45bcd2fcb718e827bc204d51fd3befd4d424e5
yogisen <36984500+yogisen@users.noreply.github.com>
2018-10-26 15:53:38 +0200

    Add files via upload

--- BasesUdemy/Devine-un-nombre-06-While.py
+++ BasesUdemy/Devine-un-nombre-06-While.py
@@ -1,42 +1,12 @@
 from random import randint
 
-range = input('Entrez la difficulté 1-10/100/100/1000/...')
-range = int(range)
+nombre_a_deviner = randint(1, 100)
 
-
-while range <= 0 or range > 1000:
-
-        if range >= 0:
-                print ('le nombre doit etre positif')
-        elif range > 1000:
-                ('le nombre est trop grand')
-                
-
-        range = input('Entrez la difficulté 1-10/100/100/1000/...')
-        range = int(range)
-        
-        
-        
-
-
-range = int(range)
-nombre_a_deviner = randint(1, range)
-
-
-
-nombre_essais = input('Combien voulez vous d essais')
-nombre_essais = int(nombre_essais)
-while nombre_essais < 1:
-        nombre_essais = input('merci d indiquer un nombre positif')
-
-
-
-        
+nombre_essais = 5
 i = 0
 while i < nombre_essais:
 
 	essai = input('Entrez un nombre ({0} essai): '.format(i + 1))
-	essai = int(essai)
 
 	if essai < nombre_a_deviner:
 		print('Le nombre a deviner est plus grand que {0}'.format(essai))
