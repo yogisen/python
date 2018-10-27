10d1142805f3dc1fd2a20a14b89707d2c2188d8c
yogisen <36984500+yogisen@users.noreply.github.com>
2018-10-27 09:27:52 +0200

    Add files via upload

--- projet liste 2.py
+++ projet liste 2.py
@@ -3,6 +3,7 @@
 # find movies with same name
 # organisze movies with alphabetic name (asc order)
 # print list
+# add nmovies start
 
 again ='o' # continuer
 movies_table = []
@@ -10,11 +11,11 @@ movies_table = []
 while again == 'o':
     movie_to_add = input('please enter a movie : ')
 
-
-    if movie_to_add.lower() in [movie.lower() for movie in movies_table]:
+    if movie_to_add.lower() in [movie[0].lower() for movie in movies_table]:
         print('{0} in DEJA a table '.format(movie_to_add))
     else:
-        movies_table.append(movie_to_add)
+        note = input(' enter one note on 5 for this movie : ')
+        movies_table.append((movie_to_add, note))
 
     again = input('do you want to continued ? o/n :')
     print('')
