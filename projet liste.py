46ebaa37fa6fdf5f52bed58f4febc8fef352b822
yogisen <36984500+yogisen@users.noreply.github.com>
2018-10-27 08:52:39 +0200

    Add files via upload

---
+++ projet liste.py
@@ -0,0 +1,22 @@
+# ask to user to enter the movies names
+# add movie in one list
+# find movies with same name
+# organisze movies with alphabetic name (asc order)
+# print list
+
+again ='o' # continuer
+movies_table = []
+
+while again == 'o':
+    movie_to_add = input('please enter a movie : ')
+    if movie_to_add in movies_table: 
+        print('{0} in DEJA a table '.format(movie_to_add))
+    else:
+        movies_table.append(movie_to_add)
+
+    again = input('do you want to continued ? o/n :')
+    print('')
+
+movies_table.sort()
+print(movies_table)
+
