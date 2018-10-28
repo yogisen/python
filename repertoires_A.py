8e894c3ed3c40baab62e7d6cc9be0b961e05dc3d
yogisen <36984500+yogisen@users.noreply.github.com>
2018-10-28 09:20:40 +0100

    Add files via upload

--- repertoires_A.py
+++ repertoires_A.py
@@ -1,12 +1,26 @@
 import os
 import json
 
+
+def print_hierarchie(fichier_json):
+    with open(fichier_json, 'r') as f:
+        structure = json.load(f)
+    print('les dossier existent deja. ')
+    print('Voici la hierarchie de dossier')
+    for key, values in structure.items():
+        print('. {0}'.format(key))
+        for value in values:
+            print ('----{0}'.format(value))
+
+        print('='*25)
+
 def creer_dossier(dossiers):
     for key, values in dossiers.items():
         for value in values:
             dossier = '{0}/{1}/{2}'.format(base, key, value)
             print('creation du dossier{0}'.format(dossier))
             os.makedirs(dossier)
+
 def ecrire_json(fichier_json, dictionnaire):
     with open(fichier_json, 'w') as  f:
         json.dump(dictionnaire, f, indent=4)
@@ -24,5 +38,8 @@ Structure = {
             'Cours': ['Python', 'django']
             }
 
-creer_dossier(Structure)
-ecrire_json(fichier_json, Structure)
+if os.path.isfile(fichier_json):
+    print_hierarchie(fichier_json)
+else:
+    creer_dossier(Structure)
+    ecrire_json(fichier_json, Structure)
