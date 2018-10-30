4597cd186cdd5b43c682d3f4bf5ba0ba40422498
yogisen <36984500+yogisen@users.noreply.github.com>
2018-10-30 09:14:22 +0100

    Add files via upload

--- MOOC FUN/moocFunPoo_1_specialMethod.py
+++ MOOC FUN/moocFunPoo_1_specialMethod.py
@@ -1,6 +1,6 @@
 s = "Je fais un MOOC sur Python"
 
-class phrase:
+class Phrase:
     def __init__(self, ma_phrase):
         self.ma_phrase = ma_phrase
         self.mots = ma_phrase.split()
@@ -17,4 +17,19 @@ class phrase:
     def __str__(self):
         return  "\n".join(self.mots)
 
-    
+class PhraseSansCasse(Phrase):
+    def __init__(self, ma_phrase):
+        Phrase.__init__(self, ma_phrase)
+        self.mots_lower = {m.lower() for m in self.mots}
+
+    def __contains__(self, mot):
+        return mot.lower() in self.mots_lower
+
+
+
+
+                           
+        
+        
+
+
