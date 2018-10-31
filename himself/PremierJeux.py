05bda9738e424c5d33a40ce75203da3e92c72758
yogisen <36984500+yogisen@users.noreply.github.com>
2018-10-31 08:37:46 +0100

    Add files via upload

--- himself/PremierJeux.py
+++ himself/PremierJeux.py
@@ -1,12 +1,27 @@
+# definir une fonction devine ?
+# A la moitié des essais demander l'aide d'un ami =
+    # 1) chaine de caractere de la taille de nbAdeviner
+    # 2) nbAdeviner/2 +3
+    # 3) le nbre ecrit à l'envers
+# aller voir la doc du input
+
+
 from random import randint
 
 print('bienvenue dans le jeu du nombre à deviner')
-level = input('select your level : 10, 100, 1000')
+level = 0
+
+while level != 10 and 100 and 1000:
+    level = input('select your level : 10, 100, 1000')
+    level = int(level)
+# comment ne pas avoir a convertir un imput(google)
 
 nb_a_deviner = randint(1,int(level))
 
 nbre_de_chance = input ('nbr de chance ? ')
 nbre_de_chance = int(nbre_de_chance)
+# comment ne pas avoir a convertir un imput(google)
+
 derniere_chance = 0
 print('le nombre à deviner est ' + str(nb_a_deviner))
 
@@ -37,8 +52,10 @@ if nb_a_deviner != votre_valeur:
 
         votre_valeur = input('derniere chance')
         votre_valeur = int(votre_valeur)
-        nb_a_deviner == votre_valeur
-        print('bravo')
+        if nb_a_deviner == votre_valeur:
+            print('bravo')
+        else:
+            print ('give me more money')
 
 
 
