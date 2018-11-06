6eb21b4bf5ea854670f3dc5ed753da1481406ffd
yogisen <36984500+yogisen@users.noreply.github.com>
2018-11-06 21:01:18 +0100

    Add files via upload

--- Django 2 - Python/Django/hellodjango/apptwo/views.py
+++ Django 2 - Python/Django/hellodjango/apptwo/views.py
@@ -9,22 +9,20 @@ def djangorocks(request):
 
 
 def picture_detail(request, category, year=0, month=0, day=0):
-    template = loader.get_template('apptwo/pictures.html')
-
-    picture = {
-        'filename': 'mountain.jpg',
-        'categories': ['color', 'landscape' ,],
-    }
-
     context = {
-        'title': "this is the picture detail",
-        'category': category,
-        'year': year,
-        'month': month,
-        'day': day,
-        'picture': picture,
+        'stylesheet': 'css/style.css',
+        'pictures': [
+            {
+                'name': 'Duck',
+                'filename': 'duck.jpg',
+            },
+            {
+                'name': 'Mountain',
+                'filename': 'mountain.jpg',
+            },
+        ],
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "apptwo/index.html", context)
 
 
 
