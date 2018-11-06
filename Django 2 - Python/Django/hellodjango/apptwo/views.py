7cb8314dd5c15ed19983a4fb0182c6bd76627d7f
yogisen <36984500+yogisen@users.noreply.github.com>
2018-11-06 17:58:26 +0100

    Add files via upload

--- Django 2 - Python/Django/hellodjango/apptwo/views.py
+++ Django 2 - Python/Django/hellodjango/apptwo/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -7,8 +9,23 @@ def djangorocks(request):
 
 
 def picture_detail(request, category, year=0, month=0, day=0):
-    body = "Category={}, year={}, month={}, day={}".format(category, year, month, day)
-    return HttpResponse(body)
+    template = loader.get_template('apptwo/pictures.html')
+
+    picture = {
+        'filename': 'mountain.jpg',
+        'categories': ['color', 'landscape' ,],
+    }
+
+    context = {
+        'page_title': "this is the picture detail",
+        'description': "<p>This <b>picture</b> was take on Mount Fuji</p>",
+        'category': category,
+        'year': year,
+        'month': month,
+        'day': day,
+        'picture': picture,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 
