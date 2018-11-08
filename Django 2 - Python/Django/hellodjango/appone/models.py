c949084107c5cdcda842de7b72fd43f54c52eccb
yogisen <36984500+yogisen@users.noreply.github.com>
2018-11-08 09:25:50 +0100

    Add files via upload

--- Django 2 - Python/Django/hellodjango/appone/models.py
+++ Django 2 - Python/Django/hellodjango/appone/models.py
@@ -1,10 +1,39 @@
 from django.db import models
 
+class Device(models.Model):
+    OS_ANDROID = 'android'
+    OS_IOS = 'ios'
 
-class Song(models.Model):
-    name = models.CharField(max_length=255)
-    duration = models.IntegerField(default=0, help_text="Duration in seconds")
-    lyrics = models.TextField(blank=True)
+    OS_CHOICES = (
+        (OS_ANDROID, 'Android'),
+        (OS_IOS, 'iOS'),
+    )
+
+    FORM_FACTOR_PHONE = 'phone'
+    FORM_FACTOR_TABLET = 'tablet'
+
+    FORM_FACTOR_CHOICES = (
+        (FORM_FACTOR_PHONE, 'Phone'),
+        (FORM_FACTOR_TABLET, 'Tablet'),
+    )
+
+    os = models.CharField(max_length=20,
+                          choices=OS_CHOICES,
+                          default=OS_ANDROID)
+    form_factor = models.CharField(max_length=20,
+                                   choices=FORM_FACTOR_CHOICES,
+                                   default=FORM_FACTOR_PHONE)
+    model = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    enabled = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return '{pk} - {os} {form_factor} {model} ({created_at} {enabled})'.format(
+            pk=self.pk,
+            os=self.get_os_display(),
+            form_factor=self.get_form_factor_display(),
+            model=self.model,
+            created_at=self.created_at,
+            enabled=self.enabled)
+
--- Django 2 - Python/Django/hellodjango/appone/views.py
+++ Django 2 - Python/Django/hellodjango/appone/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from appone.models import Song
+from appone.models import Device
+
+def devices_add(request, os, model):
+    device = Device(os=os, model=model)
+    device.save()
+    return HttpResponse("Created device {}".format(device))
 
 
-# Create your views here.
-def hello(request):
-    return HttpResponse("Hello Django! appone application")
+def devices_detail(request, id):
+    try:
+        device = Device.objects.get(id=id)
+    except Device.DoesNotExist:
+        return HttpResponse(status=404)
+
+    return HttpResponse(device)
 
 
-def songs_add(request, song_name, duration):
-    song = Song(name=song_name, duration=duration)
-    song.save()
-    return HttpResponse("Song saved at id {}".format(song.pk))
+def devices_filter(request, os):
+    devices_names = []
+    for d in Device.objects.filter(os=os):
+        devices_names.append(d.__str__())
 
-
-def songs_list(request):
-    names = []
-    for s in Song.objects.all():
-        names.append(s.name)
-
-    body = '<br/>'.join(names)
+    body = '<br/>'.join(devices_names)
     return HttpResponse(body)
