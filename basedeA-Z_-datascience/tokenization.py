f7cc73661a3553ebaa011738d071c14e791de3d9
yogisen <36984500+yogisen@users.noreply.github.com>
2018-11-23 15:31:36 +0100

    Add files via upload

--- basedeA-Z_-datascience/tokenization.py
+++ basedeA-Z_-datascience/tokenization.py
@@ -1,5 +1,23 @@
-vocabulary = open("dictionnaire.txt", "r", encoding="utf-8").read()
+# vocabulary = open("dictionnaire.txt", "r", encoding="utf-8").read()
+# tokenized_vocabulary = vocabulary.split(" ")
+# print(tokenized_vocabulary)
 
-tokenized_vocabulary = vocabulary.split(" ")
+f = open("texte.txt", "r", encoding="utf-8")
+text_string = f.read()
+# print (text_string)
 
-print(tokenized_vocabulary)
+#text_string = text_string.replace(",","")
+#text_string = text_string.replace("'","")
+#text_string = text_string.replace(".","")
+# text_string = text_string.replace("\n","")
+#print (text_string)
+
+def clean_text(string):
+    cleaned_string = string.replace(",","")
+    cleaned_string = string.replace(",","")
+    cleaned_string = string.replace(",","")
+    cleaned_string = string.replace(",","")
+    return (cleaned_string)
+
+cleaned_text = clean_text(text_string)
+print (cleaned_text)
